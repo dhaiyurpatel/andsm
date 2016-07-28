c3361fcca59e80db4dc2b19370b88a99b470fda5
sachinlanover <sachin@lanover.com>
2016-07-28 14:19:20 +0530

    Changes 14:19

--- app/src/main/java/info/androidhive/materialdesign/activity/HomeFragment.java
+++ app/src/main/java/info/androidhive/materialdesign/activity/HomeFragment.java
@@ -4,6 +4,7 @@ package info.androidhive.materialdesign.activity;
  * Created by Ravi on 29/07/15.
  */
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -29,6 +30,22 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        rootView.findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getActivity());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                Intent i = new Intent(getActivity(), WelcomeActivity.class);
+                startActivity(i);
+
+            }
+        });
 
 
         // Inflate the layout for this fragment
